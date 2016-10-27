bb99cc75ad234ce136e583ac99f4424bb0c6a10a
Anton Yurchenko <antonyu@i.ua>
2016-10-27 09:53:15 +0300

    module10-tasks

--- src/module10/MyException1.java
+++ src/module10/MyException1.java
@@ -1,15 +1,7 @@
 package module10;
 
-public class MyException1 extends Exception{
-    //private String message1;
-
+public class MyException1 extends Exception {
     public MyException1(String message) {
-        //message1 = message;
-        //System.out.println("Exception message inside constructor \"" + message1 +"\"");
         super(message);
     }
-
-    //public String getMessage1() {
-    //    return message1;
-    //}
 }
